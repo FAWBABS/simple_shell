ba993508aca16226a0282055e50cc27f5afb908e
FAWBABS <fawazbabaodutayo@gmail.com>
2022-09-07 14:17:16 +0000

    shell

---
+++ aux_error1.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * strcat_cd - Function that concatenates the message for cd error
+ * @datash: data relevant (directory)
+ * @msg: Message to print
+ * @error: Output message
+ * @ver_str: Counter lines
+ * Return: Error message
+ */
+
+char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
